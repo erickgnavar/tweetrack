c42e61132eb60d89fd20d825daf4ecd779e09ccd
Erick Navarro <erick@navarro.io>
2017-12-31 13:35:33 -0800

    Add format to search dates in index template

--- lib/tweetrack_web/views/search_view.ex
+++ lib/tweetrack_web/views/search_view.ex
@@ -1,3 +1,11 @@
 defmodule TweetrackWeb.SearchView do
   use TweetrackWeb, :view
+
+  def format_date(date) do
+    if is_nil(date) do
+      "-"
+    else
+      "#{date.day}/#{date.month}/#{date.year} #{date.hour}:#{date.minute}"
+    end
+  end
 end
